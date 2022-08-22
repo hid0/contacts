c307d6cc97cb2112d1951fbeeef347c7251097a2
hid0 <faizhid11@gmail.com>
2022-08-22 14:01:27 +0700

    learn state components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import ContactApp from "./components/ContactApp";
+// import "./styles/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = createRoot(document.getElementById("root"));
+// root.render(<ContactApp />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function CounterDisplay({ count }) {
+  if (count === 0) {
+    return <p>{count}</p>;
+  }
+
+  if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz</p>;
+  }
+
+  if (count % 5 === 0) {
+    return <p>Fizz</p>;
+  }
+
+  if (count % 7 === 0) {
+    return <p>Buzz</p>;
+  }
+
+  return <p>{count}</p>;
+}
+
+function IncraseButton({ increase }) {
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
+}
+
+function ResetButton({ reset }) {
+  return (
+    <div>
+      <button onClick={reset}>- reset</button>
+    </div>
+  );
+}
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi nilai count di dalam state
+    this.state = {
+      count: 0,
+    };
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+  }
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1,
+      };
+    });
+  }
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <IncraseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<CounterApp />);
